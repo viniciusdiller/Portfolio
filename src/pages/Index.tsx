65becd5540fced6eb925db7fd6000b2df7183408
Vinícius <viniciusdiller@hotmail.com>
2026-08-15 14:03:25 -0300

    Revert "Add sticky scroll-stack animation between hero and projects sections"
    This reverts commit 679caedcdc2780c9f4ca52dc7748ad8240d8f4e6.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,20 +8,17 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import ScrollProgress from "@/components/ScrollProgress";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
-import StickySections from "@/components/StickySections";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-background via-background to-muted/20">
       <ScrollProgress />
       <Navbar />
-      <StickySections>
-        <Hero />
-        <FeaturedScroll />
-        <RobotCTA />
-        <About />
-        <Projects />
-      </StickySections>
+      <Hero />
+      <FeaturedScroll />
+      <RobotCTA />
+      <About />
+      <Projects />
       <Contact />
       <Footer />
       <ScrollToTopButton />
